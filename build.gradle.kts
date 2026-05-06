f80fd7c1e6345568db683ae3b2626cad171e5214
CADIndie <darienmartaze8@gmail.com>
2026-05-05 19:41:54 -0500

    Update bindings

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,19 +15,13 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation(fileTree(mapOf("dir" to "src/test/libs", "include" to listOf("*.jar"))))
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    implementation("org.apache.logging.log4j:log4j-core:2.25.3")
-    implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.25.3")
     implementation("org.slf4j:slf4j-api:2.0.17")
     implementation("net.java.dev.jna:jna:5.18.1")
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 }
 
 tasks.shadowJar {
-    dependencies {
-        exclude(dependency(":org.apache.logging.log4j:log4j-core:2.25.3"))
-        exclude(dependency(":org.apache.logging.log4j:log4j-slf4j2-impl:2.25.3"))
-        exclude(dependency(":org.slf4j:slf4j-api:2.0.17"))
-    }
+    archiveClassifier.set("")
 }
 
 tasks.test {
